9b29d1f8d454ccbb05af7fa42b366baa0aa7d93c
Lucas Busnelli <lucasbusnelli19@gmail.com>
2026-09-25 15:13:05 -0300

    Nuevo
    Redireccion con codigo ekeksis, mejor sistema de padding en secciones, favicon, orden de codigo.

--- scripts/scroll-reveal.js
+++ scripts/scroll-reveal.js
@@ -8,3 +8,21 @@ window.addEventListener('scroll', () => {
     header.classList.remove('is-visible');
   }
 }, { passive: true });
+
+document.getElementById('redirect-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const inputValor = document.getElementById('name').value.trim().toLowerCase();
+    const errorMsg = document.getElementById('error-msg');
+
+    const PALABRA_SECRETA = "ekexis"; 
+    const DESTINO_URL = "https://www.instagram.com/ekangea.tdm3/";
+
+    if (inputValor === PALABRA_SECRETA) {
+        errorMsg.style.display = 'none';
+        window.location.href = DESTINO_URL;
+        //window.open(DESTINO_URL, '_blank');
+    } else {
+        errorMsg.style.display = 'block';
+    }
+});
